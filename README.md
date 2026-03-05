83f139a17e83ae07117ced28c721f91a56ad6d2b
Wojciech Dudek <wdudek82@gmail.com>
2026-03-05 21:21:51 +0100

    Update project name

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# SandboxNg14
+# Mycology Genetics Tracker
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 14.2.3.
 
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'sandbox-ng-14'`, () => {
+  it(`should have as title 'mycology-genetics-tracker'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('sandbox-ng-14');
+    expect(app.title).toEqual('mycology-genetics-tracker');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('sandbox-ng-14 app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('mycology-genetics-tracker app is running!');
   });
 });
