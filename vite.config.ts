9546d0087f182490544430f03f868cfa55cd9c85
Wojciech Dudek <wdudek82@gmail.com>
2026-03-14 13:22:02 +0100

    MGT-10: fix Vitest CSS warnings

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,11 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
   plugins: [angular(), tsconfigPaths()],
+  css: {
+    preprocessorOptions: {
+      scss: {
+        api: 'modern',
+      },
+    },
+  },
 });
